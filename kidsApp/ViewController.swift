8170b3cdedb69bdc50ea5aaa1910168197f13cba
Betty Pan <betty19931218@gmail.com>
2021-04-07 13:10:47 +0800

    Initial Commit

---
+++ kidsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kidsApp
+//
+//  Created by Betty Pan on 2021/4/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
